7c52a04f762c26cb7f29c4741f07c605842e69b2
Buck <elie@gazel.net>
2019-03-25 20:48:55 +0100

    include header

--- don.php
+++ don.php
@@ -10,43 +10,14 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.4/css/mdb.min.css" rel="stylesheet">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark brown fixed-top">
-        <a class="navbar-brand" href="#">Refuge Du Languedoc</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="./index_membre.php">Accueil
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./adopter.php">Adopter</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./collectes.php">Prochaines collectes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./don.php">Faire un Don</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./contact.php">Contact</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="./devenir_fa.php">Devenir FA</a>
-
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<script>
+    $(function(){
+        $("#header").load("./includes/header.html");
+    });
+</script>
+<div id="header"></div>
 <body>
 <div class="container h-100">
     <div class="row h-100">
