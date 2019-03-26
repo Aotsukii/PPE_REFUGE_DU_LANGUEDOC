33f35d4f8835647c7bcfcd80ba394b83ab721c63
Buck <40405549+Buck@users.noreply.github.com>
2019-03-26 18:18:01 +0100

    script/mcd db

--- db/script.sql
+++ db/script.sql
@@ -4,30 +4,40 @@
 
 
 #------------------------------------------------------------
-# Table: GERANT
-#------------------------------------------------------------
-
-CREATE TABLE GERANT(
-        id_gerant Int  Auto_increment  NOT NULL
-	,CONSTRAINT GERANT_PK PRIMARY KEY (id_gerant)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
 # Table: USER
 #------------------------------------------------------------
 
 CREATE TABLE USER(
-        id_gerant   Int NOT NULL ,
-        id_user     Int NOT NULL ,
-        login       Varchar (50) NOT NULL ,
-        password    Varchar (50) NOT NULL ,
-        mail        Varchar (50) NOT NULL ,
-        nom_user    Varchar (50) NOT NULL ,
-        prenom_user Varchar (50) NOT NULL
-	,CONSTRAINT USER_PK PRIMARY KEY (id_gerant,id_user)
+        id_user      Int  Auto_increment  NOT NULL ,
+        login        Varchar (50) NOT NULL ,
+        password     Varchar (50) NOT NULL ,
+        mail         Varchar (50) NOT NULL ,
+        nom_user     Varchar (50) NOT NULL ,
+        prenom_user  Varchar (50) NOT NULL ,
+        adresse_user Varchar (50) NOT NULL ,
+        ville_user   Varchar (50) NOT NULL ,
+        tel_user     Varchar (50) NOT NULL
+	,CONSTRAINT USER_PK PRIMARY KEY (id_user)
+)ENGINE=InnoDB;
 
-	,CONSTRAINT USER_GERANT_FK FOREIGN KEY (id_gerant) REFERENCES GERANT(id_gerant)
+
+#------------------------------------------------------------
+# Table: GERANT
+#------------------------------------------------------------
+
+CREATE TABLE GERANT(
+        id_user      Int NOT NULL ,
+        login        Varchar (50) NOT NULL ,
+        password     Varchar (50) NOT NULL ,
+        mail         Varchar (50) NOT NULL ,
+        nom_user     Varchar (50) NOT NULL ,
+        prenom_user  Varchar (50) NOT NULL ,
+        adresse_user Varchar (50) NOT NULL ,
+        ville_user   Varchar (50) NOT NULL ,
+        tel_user     Varchar (50) NOT NULL
+	,CONSTRAINT GERANT_PK PRIMARY KEY (id_user)
+
+	,CONSTRAINT GERANT_USER_FK FOREIGN KEY (id_user) REFERENCES USER(id_user)
 )ENGINE=InnoDB;
 
 
@@ -36,19 +46,24 @@ CREATE TABLE USER(
 #------------------------------------------------------------
 
 CREATE TABLE FA(
-        id_gerant      Int NOT NULL ,
-        id_fa          Int NOT NULL ,
+        id_user        Int NOT NULL ,
         type_Logement  Varchar (50) NOT NULL ,
         aChien         Bool NOT NULL ,
         aChat          Bool NOT NULL ,
         aEnfant        Bool NOT NULL ,
         ok_Quarantaine Bool NOT NULL ,
         espace_Dispo   Int NOT NULL ,
-        adresse_fa     Varchar (50) NOT NULL ,
-        ville_fa       Varchar (50) NOT NULL
-	,CONSTRAINT FA_PK PRIMARY KEY (id_gerant,id_fa)
+        login          Varchar (50) NOT NULL ,
+        password       Varchar (50) NOT NULL ,
+        mail           Varchar (50) NOT NULL ,
+        nom_user       Varchar (50) NOT NULL ,
+        prenom_user    Varchar (50) NOT NULL ,
+        adresse_user   Varchar (50) NOT NULL ,
+        ville_user     Varchar (50) NOT NULL ,
+        tel_user       Varchar (50) NOT NULL
+	,CONSTRAINT FA_PK PRIMARY KEY (id_user)
 
-	,CONSTRAINT FA_GERANT_FK FOREIGN KEY (id_gerant) REFERENCES GERANT(id_gerant)
+	,CONSTRAINT FA_USER_FK FOREIGN KEY (id_user) REFERENCES USER(id_user)
 )ENGINE=InnoDB;
 
 
@@ -57,14 +72,18 @@ CREATE TABLE FA(
 #------------------------------------------------------------
 
 CREATE TABLE MEMBRE(
-        id_gerant      Int NOT NULL ,
-        id_membre      Int NOT NULL ,
-        adresse_membre Varchar (50) NOT NULL ,
-        ville_membre   Varchar (50) NOT NULL ,
-        Tel            Varchar (50) NOT NULL
-	,CONSTRAINT MEMBRE_PK PRIMARY KEY (id_gerant,id_membre)
+        id_user      Int NOT NULL ,
+        login        Varchar (50) NOT NULL ,
+        password     Varchar (50) NOT NULL ,
+        mail         Varchar (50) NOT NULL ,
+        nom_user     Varchar (50) NOT NULL ,
+        prenom_user  Varchar (50) NOT NULL ,
+        adresse_user Varchar (50) NOT NULL ,
+        ville_user   Varchar (50) NOT NULL ,
+        tel_user     Varchar (50) NOT NULL
+	,CONSTRAINT MEMBRE_PK PRIMARY KEY (id_user)
 
-	,CONSTRAINT MEMBRE_GERANT_FK FOREIGN KEY (id_gerant) REFERENCES GERANT(id_gerant)
+	,CONSTRAINT MEMBRE_USER_FK FOREIGN KEY (id_user) REFERENCES USER(id_user)
 )ENGINE=InnoDB;
 
 
@@ -76,11 +95,10 @@ CREATE TABLE DON(
         id_don      Int  Auto_increment  NOT NULL ,
         montant_don Int NOT NULL ,
         date_don    Date NOT NULL ,
-        id_gerant   Int NOT NULL ,
-        id_membre   Int NOT NULL
+        id_user     Int NOT NULL
 	,CONSTRAINT DON_PK PRIMARY KEY (id_don)
 
-	,CONSTRAINT DON_MEMBRE_FK FOREIGN KEY (id_gerant,id_membre) REFERENCES MEMBRE(id_gerant,id_membre)
+	,CONSTRAINT DON_MEMBRE_FK FOREIGN KEY (id_user) REFERENCES MEMBRE(id_user)
 )ENGINE=InnoDB;
 
 
@@ -126,32 +144,14 @@ CREATE TABLE ANIMAL(
         pelage_animal      Varchar (50) NOT NULL ,
         besoinTraitement   Bool NOT NULL ,
         est_chat           Bool NOT NULL ,
-        id_gerant          Int NOT NULL ,
-        id_gerant_FA       Int ,
-        id_fa              Int ,
-        id_gerant_MEMBRE   Int NOT NULL ,
-        id_membre          Int NOT NULL ,
+        id_user            Int ,
+        id_user_MEMBRE     Int NOT NULL ,
         id_race            Int NOT NULL
 	,CONSTRAINT ANIMAL_PK PRIMARY KEY (id_animal)
 
-	,CONSTRAINT ANIMAL_GERANT_FK FOREIGN KEY (id_gerant) REFERENCES GERANT(id_gerant)
-	,CONSTRAINT ANIMAL_FA0_FK FOREIGN KEY (id_gerant_FA,id_fa) REFERENCES FA(id_gerant,id_fa)
-	,CONSTRAINT ANIMAL_MEMBRE1_FK FOREIGN KEY (id_gerant_MEMBRE,id_membre) REFERENCES MEMBRE(id_gerant,id_membre)
-	,CONSTRAINT ANIMAL_RACE2_FK FOREIGN KEY (id_race) REFERENCES RACE(id_race)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: ORGANISER
-#------------------------------------------------------------
-
-CREATE TABLE ORGANISER(
-        id_collecte Int NOT NULL ,
-        id_gerant   Int NOT NULL
-	,CONSTRAINT ORGANISER_PK PRIMARY KEY (id_collecte,id_gerant)
-
-	,CONSTRAINT ORGANISER_COLLECTE_FK FOREIGN KEY (id_collecte) REFERENCES COLLECTE(id_collecte)
-	,CONSTRAINT ORGANISER_GERANT0_FK FOREIGN KEY (id_gerant) REFERENCES GERANT(id_gerant)
+	,CONSTRAINT ANIMAL_FA_FK FOREIGN KEY (id_user) REFERENCES FA(id_user)
+	,CONSTRAINT ANIMAL_MEMBRE0_FK FOREIGN KEY (id_user_MEMBRE) REFERENCES MEMBRE(id_user)
+	,CONSTRAINT ANIMAL_RACE1_FK FOREIGN KEY (id_race) REFERENCES RACE(id_race)
 )ENGINE=InnoDB;
 
 
@@ -161,11 +161,10 @@ CREATE TABLE ORGANISER(
 
 CREATE TABLE PARTICIPER(
         id_collecte Int NOT NULL ,
-        id_gerant   Int NOT NULL ,
-        id_membre   Int NOT NULL
-	,CONSTRAINT PARTICIPER_PK PRIMARY KEY (id_collecte,id_gerant,id_membre)
+        id_user     Int NOT NULL
+	,CONSTRAINT PARTICIPER_PK PRIMARY KEY (id_collecte,id_user)
 
 	,CONSTRAINT PARTICIPER_COLLECTE_FK FOREIGN KEY (id_collecte) REFERENCES COLLECTE(id_collecte)
-	,CONSTRAINT PARTICIPER_MEMBRE0_FK FOREIGN KEY (id_gerant,id_membre) REFERENCES MEMBRE(id_gerant,id_membre)
+	,CONSTRAINT PARTICIPER_MEMBRE0_FK FOREIGN KEY (id_user) REFERENCES MEMBRE(id_user)
 )ENGINE=InnoDB;
 
